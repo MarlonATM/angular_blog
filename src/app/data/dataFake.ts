0876e40d1e6241fc13ed108e6f8c67c3fd3d734c
Marlon Matos <marlon-am@hotmail.com>
2024-02-18 20:40:26 +0000

    Content created and searched from fake data

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":1,
+        "image":"https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png",
+        "title":"MARVEL ANUNCIA",
+        "description":"Novo filme da saga ultron"
+    },
+    {
+        "id":2,
+        "image":"https://th.bing.com/th/id/R.52406f9fd62de6271fb76e372115cd3b?rik=IyT1%2fVhksFcmXQ&riu=http%3a%2f%2fradiusblocks.com%2fwp-content%2fuploads%2f2022%2f09%2fimage-grid_5.jpg&ehk=J60uhjobAJSrVLuIJuQHckdkUSobwjLg%2bdQqCRVmaik%3d&risl=&pid=ImgRaw&r=0",
+        "title":"TESTE NOTICIA NOVA",
+        "description":"Saiu agora uma noticia quentinha direto do forno"
+    },
+    {
+        "id":3,
+        "image":"https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png",
+        "title":"OLHA ESTA NOTICIA",
+        "description":"bla blabsdjlabduo abdaalef bfiobsbf oiisnfiod ebnfsifn"
+    },
+    {
+        "id":4,
+        "image":"https://www.pngkey.com/png/detail/233-2332677_image-500580-placeholder-transparent.png",
+        "title":"JUSCILAINE TODDYNHO DE BIQUINI NA PRAIA",
+        "description":"Não perca as imagens a seguir"
+    },
+]
